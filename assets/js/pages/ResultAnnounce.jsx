74dac12efda5d8f4712cd98a8358e2a9d2832363
ThibautRingeval <thibaut.ringeval@epsi.fr>
2023-10-05 09:59:56 +0200

    ads

--- assets/js/pages/ResultAnnounce.jsx
+++ assets/js/pages/ResultAnnounce.jsx
@@ -1,26 +1,32 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
+import "./App.css"; // Assurez-vous d'avoir un fichier CSS pour les styles
 
-authAPI.setup();
+function App() {
+  const [count, setCount] = useState(0);
 
-const App = () => {
-  const [isAuthenticated, setIsAuthenticated] = useState(
-    authAPI.isAuthenticated()
-  );
-
-  const NavbarWithRouter = withRouter(Navbar);
+  const handleFilterClick = () => {
+    // Ajoutez ici la logique pour gérer le filtre
+  };
 
   return (
-    <HashRouter>
-      <NavbarWithRouter />
-      <main className="container pt-5">
-        <Switch>
-          <Route path="/login" component={LoginPage} />
-          <Route path="/register" component={RegisterPage} />
-          <Route path="/ResultAnnonce" component={ResultAnnounce} />
-        </Switch>
-      </main>
-    </HashRouter>
+    <div className="App">
+      {/* Carré rouge en haut */}
+      <div className="header">
+        <div className="red-square"></div>
+      </div>
+
+      {/* Bouton de filtre à droite */}
+      <div className="filter-button">
+        <button onClick={handleFilterClick}>Filtrer</button>
+      </div>
+
+      {/* Compteur */}
+      <div className="counter">
+        <p>Compteur : {count}</p>
+        <button onClick={() => setCount(count + 1)}>Incrémenter</button>
+      </div>
+    </div>
   );
-};
+}
 
 export default App;
